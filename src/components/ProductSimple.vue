8a28e4232cc9b83b6cb587bfe47034cd395d9060
pcharli <pcharli@heh.be>
2024-12-19 14:37:05 +0100

    add component

---
+++ src/components/ProductSimple.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="card">
+            
+            <div class="img-container">
+              <img :src="produit.img" />
+            </div>
+  
+            <div class="card-text">
+              <h3>{{ produit.description }}</h3>
+              <span>{{ produit.price }} €</span>
+            </div>
+
+            <div class="card-icons">
+              <div class="like-container">
+                <input
+                  type="checkbox"
+                  :value="produit"
+                  name="checkbox"
+                  :id="produit.id"
+                />
+                <label :for="produit.id">
+                  <i class="fas fa-heart" @click="addToLiked(produit)"></i>
+                </label>
+              </div>
+  
+              <div class="add-to-cart">
+                <button @click="addToCart(produit)">
+                  <i class="fas fa-shopping-cart"></i>
+                </button>
+              </div>
+            </div>
+
+          </div>
+</template>
+
+<script setup>
+    //import {defineProps, defineEmits} from 'vue'
+
+
+    const props = defineProps({
+        produit: Object
+    })
+
+    const emits = defineEmits([
+        'addToCart', 'addToLiked'
+    ])
+
+    const addToCart = (produit) => {
+        emits('addToCart', produit)
+    }
+    const addToLiked = (produit) => {
+        emits('addToLiked', produit)
+    }
+
+</script>
+
+<style lang="scss">
+$color: red;
+.add-to-cart {
+  background-color: $color;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,50 @@
 <script setup>
+import {ref} from 'vue'
+import Product from '@/components/ProductSimple.vue'
 
+const productsList = ref([
+    { id: 1, description: "Quarz Luxe", price: 12, img: "https://i.ibb.co/B4grBGR/quarz-luxe.jpg"},
+    { id: 2, description: 'Curren Business', price: 20, img: "https://i.ibb.co/n6GFgv6/curren-business.jpg"},
+    { id: 3, description: 'Curren Sport', price: 5, img: "https://i.ibb.co/Y74GB7y/curren-sport.jpg" },
+    { id: 4, description: 'Jaragar Racing', price: 8, img: "https://i.ibb.co/XXwP7cb/jaragar-racing.jpg" },
+    { id: 5, description: 'Liges Hommes', price: 3, img: "https://i.ibb.co/x5ZnVwj/liges-hommes.jpg"},
+    { id: 6, description: 'Maserati Mechanical', price: 65, img: "https://i.ibb.co/NSWnpjS/maserati-mechanical.jpg"},
+    { id: 7, description: 'Montre Mecanique', price: 25, img: "https://i.ibb.co/cN8B3rm/montre-mecanique.jpg" },
+    { id: 8, description: 'Brand Designer', price: 28, img: 'https://i.ibb.co/Pmtyp6R/brand-designer.jpg'},
+    { id: 9, description: 'Relogio Masculino', price: 4, img: "https://i.ibb.co/WnS3d6h/relogio-masculino.jpg"},
+    { id: 10, description: 'Tissot Multifunction', price: 29, img: "https://i.ibb.co/FD6wb7p/tissot-multifunction.jpg"},
+    { id: 11, description: 'Hip Hop Gold', price: 87, img: "https://i.ibb.co/WvVw5W7/hiphop-gold.jpg"},
+    { id: 12, description: 'Mesh Genova', price: 6, img: "https://i.ibb.co/qdHmbWJ/mesh-genova.jpg" }
+  ])
+
+  const productsLiked = ref([])
+
+  const productsCart = ref([])
+
+  const addToCart = (produit) => {
+    productsCart.value.push({ ...produit})
+  }
+  const addToLiked = (produit) => {
+    let index = (productsLiked.value.findIndex(e => e.id == produit.id))
+    if(index < 0 )  {
+      productsLiked.value.push({ ...produit})
+    }
+    else {
+      productsLiked.value.splice(index,1)
+    }
+  }
 </script>
 
 <template>
   <main>
-    <h1>Home</h1>
+  <div class="home-container">
+    <h1>Nos produits</h1>
+    {{  productsLiked }}
+    <div class="card-cart-container">
+      <div class="card-container">
+        <Product @addToLiked="addToLiked" @addToCart="addToCart" v-for="(produit, id) in productsList" :key="id" :produit="produit"></Product>
+      </div>
+    </div>
+  </div>
   </main>
 </template>
